2c64eacae876ab15b5ca2986cb1d602c2105fb91
MDucazeaux <m.ducazeaux@lahorde.school>
2022-04-04 20:28:39 +0200

    Add Node.cs Tree.cs Selector.cs Sequence.cs
    Add every composant needed to create a behavior tree

--- Offensive_CWMAQ/Assets/Scripts/BehaviorTree/Node.cs
+++ Offensive_CWMAQ/Assets/Scripts/BehaviorTree/Node.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Node : MonoBehaviour
+namespace BehaviorTree
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public enum NodeState
     {
-        
+        RUNNIG,
+        SUCCESS,
+        FAILURE
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Node
     {
-        
+        protected NodeState state;
+
+        public Node parent;
+
+        protected List<Node> children = new List<Node>();
+
+
+        public Node()
+        {
+            parent = null;
+        }
+
+        public Node(List<Node> children)
+        {
+            foreach (Node child in children)
+                Link(child);
+        }
+
+        private void Link(Node node)
+        {
+            node.parent = this;
+            children.Add(node);
+        }
+
+        public virtual NodeState Evaluate() => NodeState.FAILURE;
+
+        private Dictionary<string, object> dataContext = new Dictionary<string, object>();
+
+        public void SetData(string key, object value)
+        {
+            dataContext[key] = value;
+        }
+
+        public object GetData(string key)
+        {
+            object value = null;
+            if (dataContext.TryGetValue(key, out value))
+                return value;
+            Node node = parent;
+            while (node != null)
+            {
+                value = node.GetData(key);
+                if (value != null)
+                    return value;
+                node = node.parent;
+            }
+            return null;
+        }
+
+        public bool ClearData(string key)
+        {
+            if (dataContext.ContainsKey(key))
+            {
+                dataContext.Remove(key);
+                return true;
+            }
+
+
+            Node node = parent;
+            while (node != null)
+            {
+                bool cleared = node.ClearData(key);
+                if (cleared)
+                    return true;
+                node = node.parent;
+
+            }
+            return false;
+
+        }
+
     }
 }
+

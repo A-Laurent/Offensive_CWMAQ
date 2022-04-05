99bdc7e614702ed39340d1ad7c0c054e81bb98e4
QRigagneau <92300152+QRigagneau@users.noreply.github.com>
2022-04-05 16:52:01 +0200

    Settings in MenuScene

--- Offensive_CWMAQ/Assets/Scripts/SceneManager/ButtonManager.cs
+++ Offensive_CWMAQ/Assets/Scripts/SceneManager/ButtonManager.cs
@@ -11,13 +11,11 @@ public class ButtonManager : MonoBehaviour
     //Those two fonctions allow the MainMenu to work correctly, QUIT to leave the game and Play to switch to the MainScene
     public void QuitButton()
     {
-        myText.text = "QUIT";
         Application.Quit();
     }
   
     public void PlayButton()
     {
-        myText.text = "Play";
         SceneManager.LoadScene("MainScene");        
     }
 }

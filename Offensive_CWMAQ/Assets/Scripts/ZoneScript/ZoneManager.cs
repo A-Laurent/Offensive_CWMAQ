c966c983a54cbc590fa9ae897f1af1a19c071a6e
MDucazeaux <m.ducazeaux@lahorde.school>
2022-04-05 21:27:00 +0200

    add wall for zone and assets
    assets to project word on the surfaces

---
+++ Offensive_CWMAQ/Assets/Scripts/ZoneScript/ZoneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneManager : MonoBehaviour
+{
+
+    private float radius;
+    private float shrinkRadius;
+    private float timeShrink;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

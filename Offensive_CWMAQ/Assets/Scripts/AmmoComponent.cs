21ac4ec916dd7eab163e16e30d0570ad514c95a2
A-Laurent <a.laurent@lahorde.school>
2022-04-05 15:57:04 +0200

    player shoot ammomanager

---
+++ Offensive_CWMAQ/Assets/Scripts/AmmoComponent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoComponent : MonoBehaviour
+{
+    public int Ammo;
+    void Start()
+    {
+        Ammo = 150;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

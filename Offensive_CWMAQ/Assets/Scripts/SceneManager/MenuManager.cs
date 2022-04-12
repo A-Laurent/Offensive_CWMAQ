95653e49c2da8202846f50dc0edaec292254a86f
QRigagneau <92300152+QRigagneau@users.noreply.github.com>
2022-04-13 01:17:10 +0200

    Synopsis
    Add Synopsis to Menu

--- Offensive_CWMAQ/Assets/Scripts/SceneManager/MenuManager.cs
+++ Offensive_CWMAQ/Assets/Scripts/SceneManager/MenuManager.cs
@@ -10,8 +10,17 @@ public class MenuManager : MonoBehaviour
     public GameObject PlayButton;
     public GameObject QuitButton;
     public GameObject Credits;
+
     public GameObject Map1;
     public GameObject Map2;
+    public GameObject SynopsisMenuBTN;
+    public GameObject ReturnBTN;
+
+    public GameObject Head;
+    public GameObject Body;
+    public GameObject Foot;
+    public GameObject SynopsisBG;
+    public GameObject ReturnMenuBTN;
 
     public EventSystem M_eventSystem;
 
@@ -20,20 +29,43 @@ public class MenuManager : MonoBehaviour
     {
         Map1.SetActive(false);
         Map2.SetActive(false);
+        ReturnBTN.SetActive(false);
+
+        Head.SetActive(false);
+        Body.SetActive(false);
+        Foot.SetActive(false);
+        SynopsisBG.SetActive(false);
+        ReturnMenuBTN.SetActive(false);
     }
 
     //Active mapselector and disable menuUI
     public void Play()
     {
         PlayButton.SetActive(false);
+        SynopsisMenuBTN.SetActive(false);
         QuitButton.SetActive(false);
         Credits.SetActive(false);
 
         Map1.SetActive(true);
         Map2.SetActive(true);
+        ReturnBTN.SetActive(true);
 
         M_eventSystem.SetSelectedGameObject(Map1);
         
+    }
+
+    public void Return()
+    {
+        PlayButton.SetActive(true);
+        QuitButton.SetActive(true);
+        SynopsisMenuBTN.SetActive(true);
+        Credits.SetActive(true);
+
+        Map1.SetActive(false);
+        Map2.SetActive(false);
+        ReturnBTN.SetActive(false);
+
+        M_eventSystem.SetSelectedGameObject(PlayButton);
     }
 
     //Load Map1
@@ -46,6 +78,20 @@ public class MenuManager : MonoBehaviour
     public void Carte2()
     {
         SceneManager.LoadScene("Carte2");
+    }
+
+    public void Synopsis()
+    {
+        PlayButton.SetActive(false);
+        SynopsisMenuBTN.SetActive(false);
+        QuitButton.SetActive(false);
+        Credits.SetActive(false);
+
+        Head.SetActive(true);
+        Body.SetActive(true);
+        Foot.SetActive(true);
+        SynopsisBG.SetActive(true);
+        ReturnMenuBTN.SetActive(true);
     }
 
     //Quit the game

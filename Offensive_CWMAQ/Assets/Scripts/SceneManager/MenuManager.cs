14df890c3f5f5b62fe890838cbcfe5f09d650b7c
QRigagneau <92300152+QRigagneau@users.noreply.github.com>
2022-04-12 12:16:08 +0200

    Main Merge
    Add Menu & Waypoints

--- Offensive_CWMAQ/Assets/Scripts/SceneManager/MenuManager.cs
+++ Offensive_CWMAQ/Assets/Scripts/SceneManager/MenuManager.cs
@@ -11,17 +11,12 @@ public class MenuManager : MonoBehaviour
     public GameObject Credits;
     public GameObject Map1;
     public GameObject Map2;
-    public GameObject LoadingScreen;
-
-    public Slider slider;
-    public Text LoadingText;
 
     //Disable Map selector on Awake
     private void Start()
     {
         Map1.SetActive(false);
         Map2.SetActive(false);
-        LoadingScreen.SetActive(false);
     }
 
     //Active mapselector and disable menuUI
@@ -36,15 +31,15 @@ public class MenuManager : MonoBehaviour
     }
 
     //Load Map1
-    public void Carte1(int sceneIndex)
+    public void Carte1()
     {
-        StartCoroutine(LoadAsync(sceneIndex));
+        SceneManager.LoadScene("Carte1");
     }
     
     //Load Map2
-    public void Carte2(int sceneIndex)
+    public void Carte2()
     {
-        StartCoroutine(LoadAsync(sceneIndex));
+        SceneManager.LoadScene("Carte2");
     }
 
     //Quit the game
@@ -63,21 +58,5 @@ public class MenuManager : MonoBehaviour
     public void GoCredits()
     {
         SceneManager.LoadScene("CréditsScene");
-    }
-
-    IEnumerator LoadAsync(int sceneIndex)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-
-        LoadingScreen.SetActive(true);
-
-        while (!operation.isDone)
-        {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            slider.value = progress;
-            LoadingText.text = progress * 100 + "%";
-
-            yield return null;
-        }
     }
 }
